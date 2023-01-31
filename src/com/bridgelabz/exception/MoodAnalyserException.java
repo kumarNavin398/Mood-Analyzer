d65c0895e5bf9e5554aa1702a420406104c27246
KumarNavin <navins398@gmail.com>
2023-01-31 17:14:59 +0530

    inform_User

---
+++ src/com/bridgelabz/exception/MoodAnalyserException.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.exception;
+
+
+public class MoodAnalyserException extends Exception {
+    public MoodAnalyserException() {
+        super();
+    }
+    public MoodAnalyserException(String message) {
+        super(message);
+    }
+}
